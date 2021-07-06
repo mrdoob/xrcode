6ef044d51ca9c394de77a2ea5ed8d888354bd48c
Mr.doob <info@mrdoob.com>
2021-07-06 23:39:00 +0100

    Added code from glitch

---
+++ README.md
@@ -0,0 +1,23 @@
+# WebXR Live Coding
+
+I've seen many people trying to do this on the Quest but always getting blocked for one reason or another. I had to give it a go myself.
+
+Kudos to the one that started it all: [RiftSketch](https://www.youtube.com/watch?v=db-7J5OaSag) 🙏
+
+--- 
+
+#### Updates
+
+September 11, 2020
+
+* Added apartment scene
+
+May 22, 2020
+
+* Added error console
+
+May 21, 2020
+
+* Made sketches clonable (squeeze button)
+* Made sketches draggable
+* First release
---
+++ js/console.js
@@ -0,0 +1,37 @@
+import * as THREE from 'https://unpkg.com/three@0.116.1/build/three.module.js';
+
+function ConsoleTexture( width, height ) {
+
+	const canvas = document.createElement( 'canvas' );
+	canvas.width = width;
+	canvas.height = height;
+
+	const context = canvas.getContext( '2d' );
+	context.fillStyle = '#000';
+	context.fillRect( 0, 0, width, height );
+
+	const texture = new THREE.CanvasTexture( canvas );
+
+	return {
+		getTexture: function () {
+
+			return texture;
+
+		},
+		setText: function ( value ) {
+
+			context.fillStyle = '#000';
+			context.fillRect( 0, 0, width, height );
+
+			context.font = 'bold 24px Menlo,Lucida Console,Courier New';
+			context.fillStyle = "#fff";
+			context.fillText( value, 20, 40 );
+
+			texture.needsUpdate = true;
+
+		}
+	}
+
+}
+
+export { ConsoleTexture }
