74abbc8689131d176f64a8319790cf3d7f143fd4
Jeff Scott Ward <jeffscottward@users.noreply.github.com>
2023-09-03 18:56:05 -0400

    prettyprint js files

--- js/console.js
+++ js/console.js
@@ -1,37 +1,31 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 
-function ConsoleTexture( width, height ) {
+function ConsoleTexture(width, height) {
+  const canvas = document.createElement("canvas");
+  canvas.width = width;
+  canvas.height = height;
 
-	const canvas = document.createElement( 'canvas' );
-	canvas.width = width;
-	canvas.height = height;
+  const context = canvas.getContext("2d");
+  context.fillStyle = "#000";
+  context.fillRect(0, 0, width, height);
 
-	const context = canvas.getContext( '2d' );
-	context.fillStyle = '#000';
-	context.fillRect( 0, 0, width, height );
+  const texture = new THREE.CanvasTexture(canvas);
 
-	const texture = new THREE.CanvasTexture( canvas );
+  return {
+    getTexture: function () {
+      return texture;
+    },
+    setText: function (value) {
+      context.fillStyle = "#000";
+      context.fillRect(0, 0, width, height);
 
-	return {
-		getTexture: function () {
+      context.font = "bold 24px Menlo,Lucida Console,Courier New";
+      context.fillStyle = "#fff";
+      context.fillText(value, 20, 40);
 
-			return texture;
-
-		},
-		setText: function ( value ) {
-
-			context.fillStyle = '#000';
-			context.fillRect( 0, 0, width, height );
-
-			context.font = 'bold 24px Menlo,Lucida Console,Courier New';
-			context.fillStyle = "#fff";
-			context.fillText( value, 20, 40 );
-
-			texture.needsUpdate = true;
-
-		}
-	}
-
+      texture.needsUpdate = true;
+    },
+  };
 }
 
-export { ConsoleTexture }
+export { ConsoleTexture };
